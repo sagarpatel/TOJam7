33f64a80d172ef0ab654e30d20549db8096399e7
Sagar Patel <17.sagar.patel@gmail.com>
2012-05-13 04:12:07 -0400

    New materials, floor transparency, etc.

--- AlmostFinalVersion/Assets/Scripts/LightScript.cs
+++ AlmostFinalVersion/Assets/Scripts/LightScript.cs
@@ -8,6 +8,9 @@ public class LightScript : MonoBehaviour {
 
 	public AudioDirectorScript audioDirector;
 
+	public bool isColor = false;
+
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -19,9 +22,19 @@ public class LightScript : MonoBehaviour {
 	{
 
 		lightIntensityValue = audioDirector.rgbAverage * lightIntensityScale;
-
-
 		light.intensity = lightIntensityValue;
+
+		if(isColor)
+		{
+			Color tempColor = new Color(audioDirector.colorArray[0], audioDirector.colorArray[1], audioDirector.colorArray[2], audioDirector.colorArray[3]);
+			light.color = tempColor;
+		}
+		else
+		{
+			light.color = new Color(1,1,1,1);
+		}
 	
+
+
 	}
 }
